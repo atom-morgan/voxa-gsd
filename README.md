82bd0f6acaccd935638f6d7903160604e9f5415f
atom-morgan <atom.morgan@gmail.com>
2014-02-09 20:48:52 -0500

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-voxa-gsd
+# Voxa GSD
+
+### Application Structure
+
+	|-- index.html
+	|-- index.php
+    |   |-- css
+    |		|-- base.css
+    |   |-- js
+    |		|-- app-start.js
+    |		|-- app.js
+    |		|-- router.js
+    |		|-- collections
+    |			|-- employee.js
+    |			|-- features.js
+    |		|-- models
+    |			|-- employee.js
+    |			|-- feature.js
+    |		|-- views
+    |			|-- employee.js
+    |			|-- employees.js
+    |			|-- feature.js
+    |			|-- features.js
+    
+#### Callouts
+
+1. index.php - This file exists simply to deploy the application to Heroku.
+2. app-start.js - This file served as my local data since a backend API was not required for this app.
+3. router.js & app.js - Routes the web app to two main views: "/team" and "/features". I defaulted the root route to show the features page similar to [Voxa's website](http://www.voxa.com).
--- js/app.js
+++ js/app.js
@@ -5,9 +5,7 @@ window.TeamManager = {
 
 	start: function(data) {
 		var features = new TeamManager.Collections.Features(data.features);
-		//console.log('break1');
 		var employees = new TeamManager.Collections.Employees(data.employees);
-		//console.log('break2');
 
 	  var router = new TeamManager.Router();
 
@@ -18,7 +16,6 @@ window.TeamManager = {
 	  	});
 	  });
 
-	  
 	  router.on('route:showEmployees', function() {
 	  	var employeesView = new TeamManager.Views.Employees({
 	  		collection: employees
@@ -37,76 +34,3 @@ window.TeamManager = {
 
 	}
 };
-
-// var directory = {
-
-//   views: {},
-
-//   models: {},
-
-//   loadTemplates: function(views, callback) {
-
-//     var deferreds = [];
-
-//     $.each(views, function(index, view) {
-//       if (directory[view]) {
-//         deferreds.push($.get('templates/' + view + '.html', function(data) {
-//             directory[view].prototype.template = _.template(data);
-//         }, 'html'));
-//       } else {
-//           alert(view + " not found");
-//       }
-//     });
-
-//     $.when.apply(null, deferreds).done(callback);
-//   }
-
-// };
-
-// directory.Router = Backbone.Router.extend({
-
-//   routes: {
-//     // A route exists for /features but I put in root to show the FeaturesView on a direct page load
-//     "":                 "features",
-//     "features":         "features",
-//     "team":             "team"
-//   },
-
-//   initialize: function () {
-//     directory.applicationView = new directory.ApplicationView();
-//     $('body').html(directory.applicationView.render().el);
-//     // Close the search dropdown on click anywhere in the UI
-//     this.$content = $("#content");
-//   },
-
-//   features: function () {
-//     if (!directory.featuresView) {
-//         directory.featuresView = new directory.FeaturesView();
-//         directory.featuresView.render();
-//     } else {
-//         console.log('reusing features view');
-//         directory.featuresView.delegateEvents();
-//     }
-//     this.$content.html(directory.featuresView.el);
-//     directory.applicationView.selectMenuItem('features-menu');
-//   },
-
-//   team: function() {
-//     var data = directoryData;
-//     if(!directory.teamView) {
-//         directory.teamView = new directory.TeamView();
-//         directory.teamView.render(data);
-//     }
-//     this.$content.html(directory.teamView.el);
-//     directory.applicationView.selectMenuItem('team-menu');
-//   }
-
-// });
-
-// $(document).on("ready", function () {
-//     directory.loadTemplates(["ApplicationView", "FeaturesView", "TeamView"],
-//         function () {
-//             directory.router = new directory.Router();
-//             Backbone.history.start();
-//         });
-// });
